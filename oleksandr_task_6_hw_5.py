486cea13cd412c6a870c36c351cc3457e898d5fd
OleksandrRabischchuk <104634962+OleksandrRabischchuk@users.noreply.github.com>
2022-05-22 08:59:48 +0300

    Add files via upload
    task_6_hw_5

---
+++ oleksandr_task_6_hw_5.py
@@ -0,0 +1,28 @@
+example_set = {1, 2, 3, 4, 'i'}
+example_set_2 = {1, 2, 3}
+#add
+example_set.add(5)
+#issubset
+example_set.issubset(example_set_2)
+#pop
+example_set.pop()
+#issuperset
+example_set.issuperset(example_set_2)
+#isdisjoint
+example_set.isdisjoint(example_set_2)
+#remove
+example_set.remove('i')
+#union
+example_set.union(example_set_2)
+#update
+example_set.update(example_set_2)
+#intersection
+example_set.intersection(example_set_2)
+#discard
+example_set.discard(2)
+#copy
+example_set.copy()
+#clear
+example_set.clear()
+#difference
+example_set.difference(example_set_2)
